ebc2b96ca76d512015b3bd8e232d923e9beee3bc
angelssj88 <angelutrera1988@gmail.com>
2022-02-16 21:45:56 -0300

    agregue un index js

---
+++ index.js
@@ -0,0 +1,17 @@
+function name(params) {
+    
+    console.log("hola mundo");
+    /* 
+        git clone <repositorio>
+        git fetch
+        git pull origin main
+        git commit -am "mensaje"
+        git push origin main
+    */
+    
+    
+    
+    
+    
+    export default name ;
+}
